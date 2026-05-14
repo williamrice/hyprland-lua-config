eb1f1311253e5a6f9eac10159c2d5fbe1c0fc588
William Rice <william@williamarice.com>
2026-05-13 22:02:56 -0400

    add hyprpaper

--- config/autostart.lua
+++ config/autostart.lua
@@ -3,8 +3,8 @@ local M = {}
 function M.setup(apps)
   hl.on("hyprland.start", function()
     hl.exec_cmd(apps.terminal)
-    hl.exec_cmd("nm-applet")
     hl.exec_cmd("waybar")
+    hl.exec_cmd("hyprpaper")
   end)
 end
 
